07bbc9d5fead4dee41ec18489f305121ab7ab4ab
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-31 14:30:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cmake/any_toolchain.cmake
@@ -0,0 +1,42 @@
+set(CMAKE_C_STANDARD   17)
+set(CMAKE_CXX_STANDARD 17)
+
+set(CMAKE_C_COMPILER_FORCED   TRUE)
+set(CMAKE_CXX_COMPILER_FORCED TRUE)
+set(CMAKE_C_COMPILER_ID       GNU)
+set(CMAKE_CXX_COMPILER_ID     GNU)
+
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_LINKER       ${CMAKE_C_COMPILER})
+set(CMAKE_OBJCOPY      ${TOOLCHAIN_PREFIX}-objcopy)
+set(CMAKE_SIZE         ${TOOLCHAIN_PREFIX}-size)
+set(CMAKE_RC_COMPILER  ${TOOLCHAIN_PREFIX}-windres)
+
+include(  os/${OS}.cmake  )
+include(arch/${ARCH}.cmake)
+include( cpu/${CPU}.cmake )
+include(  hw/${HW}.cmake  )
+
+string(TOUPPER ${HW}   HW_  )
+string(TOUPPER ${CPU}  CPU_ )
+string(TOUPPER ${ARCH} ARCH_)
+string(TOUPPER ${OS}   OS_  )
+
+add_compile_options(
+    "-D${HW_}" "-D${CPU_}" "-D${ARCH_}" "-D${OS_}"
+    -Wall -Wextra -Wpedantic
+    $<$<CONFIG:Debug>:-DDEBUG>
+)
+
+add_link_options(
+    -Wl,--print-memory-usage
+)
+
+if(CMAKE_BUILD_TYPE MATCHES Debug)
+    add_compile_options(-Og -g3)
+endif()
+if(CMAKE_BUILD_TYPE MATCHES Release)
+    add_compile_options(-Os -g0)
+endif()
---
+++ cmake/arm-none-eabi.cmake
@@ -0,0 +1,33 @@
+set(CMAKE_SYSTEM_NAME      Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(TOOLCHAIN_PREFIX       arm-none-eabi)
+set(CMAKE_CROSS_COMPILING  true)
+
+include(cmake/any_toolchain.cmake)
+
+add_compile_options(
+    -nostdlib
+    -ffunction-sections -fdata-sections
+    -DCORTEX -D${SERIES}
+    $<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
+#     $<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>
+    $<$<COMPILE_LANGUAGE:ASM>:-x$<SEMICOLON>assembler-with-cpp>
+    $<$<COMPILE_LANGUAGE:ASM>:-MMD>
+    $<$<COMPILE_LANGUAGE:ASM>:-MP>
+)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+add_link_options(
+    -nostdlib
+    -T ${CMAKE_SOURCE_DIR}/hw/${HW}/${CPU_}x_FLASH.ld
+    --specs=nano.specs
+    -Wl,--start-group -lc -lm -lnosys   -Wl,--end-group
+    -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group
+    -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections
+)
+
+set(CMAKE_EXECUTABLE_SUFFIX_ASM ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_C   ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
