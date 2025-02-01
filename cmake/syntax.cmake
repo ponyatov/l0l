929c9ad70c248b60793261185b906815749e9130
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 22:31:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/syntax.cmake
+++ cmake/syntax.cmake
@@ -9,20 +9,26 @@ file(GLOB RL
     src/*.ragel lib/src/*.ragel lib/*/src/*.ragel
 )
 
-foreach(R ${RL})
-    string(REGEX REPLACE
-        ".+\/(.+)\.ragel$"
-        "tmp/\\1.ragel.cpp"
-        RC ${R})
-    message("------- " ${R} "\t\t" ${RC})
+foreach(RAGEL_FILE ${RL})
+#     string(REGEX REPLACE
+#         ".+\/(.+)\.ragel$"
+#         "tmp/\\1.ragel.cpp"
+#         PARSER_FILE ${RAGEL_FILE})
+    message("------- " ${RAGEL_FILE} "\t\t" ${PARSER_FILE})
+#     list(APPEND CP ${PARSER_FILE})
+#     add_custom_command(OUTPUT ${PARSER_FILE}
+#         COMMAND           ${RAGEL_EXECUTABLE}
+#         ARGS              ${RAGEL_EXECUTABLE_opts} -o ${PARSER_FILE} ${RAGEL_FILE}
+#         DEPENDS           ${RAGEL_FILE}
+#         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+#     )
 endforeach()
 
+# # list(TRANSFORM RL REPLACE "[.]ragel$" ".cpp.ragel")
+# # list(TRANSFORM RL REPLACE ".+\/" "tmp/")
 
-# list(TRANSFORM RL REPLACE "[.]ragel$" ".cpp.ragel")
-# list(TRANSFORM RL REPLACE ".+\/" "tmp/")
-
-# RAGEL_TARGET(cli
-#     lib/cli/src/cli.ragel
-#     ${CMAKE_SOURCE_DIR}/tmp/cli.ragel.cpp
-#     COMPILE_FLAGS -G2
-# )
+# # RAGEL_TARGET(cli
+# #     lib/cli/src/cli.ragel
+# #     ${CMAKE_SOURCE_DIR}/tmp/cli.ragel.cpp
+# #     COMPILE_FLAGS -G2
+# # )
