835a21aeb35e6725a4fa43a233c25997d18635db
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 22:42:30 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/syntax.cmake
+++ cmake/syntax.cmake
@@ -10,18 +10,18 @@ file(GLOB RL
 )
 
 foreach(RAGEL_FILE ${RL})
-#     string(REGEX REPLACE
-#         ".+\/(.+)\.ragel$"
-#         "tmp/\\1.ragel.cpp"
-#         PARSER_FILE ${RAGEL_FILE})
-    message("------- " ${RAGEL_FILE} "\t\t" ${PARSER_FILE})
-#     list(APPEND CP ${PARSER_FILE})
-#     add_custom_command(OUTPUT ${PARSER_FILE}
-#         COMMAND           ${RAGEL_EXECUTABLE}
-#         ARGS              ${RAGEL_EXECUTABLE_opts} -o ${PARSER_FILE} ${RAGEL_FILE}
-#         DEPENDS           ${RAGEL_FILE}
-#         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-#     )
+    string(REGEX REPLACE
+        ".+\/(.+)\.ragel$"
+        "tmp/\\1.ragel.cpp"
+        PARSER_FILE ${RAGEL_FILE})
+    list(APPEND CP ${PARSER_FILE})
+    add_custom_command(
+        OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_FILE}
+        DEPENDS             ${RAGEL_FILE}
+        WORKING_DIRECTORY   ${CMAKE_SOURCE_DIR}
+        COMMAND             ${RAGEL_EXECUTABLE}
+        ARGS                ${RAGEL_EXECUTABLE_opts} -o ${PARSER_FILE} ${RAGEL_FILE}
+    )
 endforeach()
 
 # # list(TRANSFORM RL REPLACE "[.]ragel$" ".cpp.ragel")
