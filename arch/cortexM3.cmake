d86ab23547c042c503074d76912af2e1c66afbe8
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 16:32:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/cortexM3.cmake
+++ arch/cortexM3.cmake
@@ -1,6 +1,10 @@
 include(arch/cortexM.cmake)
 
+set(MCPU -march=armv7-m -mcpu=cortex-m3)
+set(FCPU -mfloat-abi=soft) # -mfpu=fpv4-sp-d16 )
+
 add_compile_options(
+    ${MCPU} ${MFPU}
 )
 
 add_compile_definitions(
@@ -9,4 +13,8 @@ add_compile_definitions(
     PREFETCH_ENABLE=1
     # INSTRUCTION_CACHE_ENABLE=1
     # DATA_CACHE_ENABLE=1
+)
+
+add_link_options(
+    ${MCPU} ${MFPU}
 )
--- arch/cortexM4.cmake
+++ arch/cortexM4.cmake
@@ -1,12 +1,18 @@
 include(arch/cortexM.cmake)
 
+set(MCPU -march=armv7e-m   -mcpu=cortex-m4)
+set(FCPU -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
+
 add_compile_options(
+    ${MCPU} ${MFPU}
 )
 
 add_compile_definitions(
-    HSI_VALUE=16000000
-    LSI_VALUE=32000
     PREFETCH_ENABLE=1
     INSTRUCTION_CACHE_ENABLE=1
     DATA_CACHE_ENABLE=1
+)
+
+add_link_options(
+    ${MCPU} ${MFPU}
 )
