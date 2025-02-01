c8fcd0684cb14bfd61687b7644091dd6b659c31f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 20:57:38 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/syntax.cmake
+++ cmake/syntax.cmake
@@ -2,6 +2,11 @@ find_package(RAGEL REQUIRED)
 find_package(FLEX  REQUIRED)
 find_package(BISON REQUIRED)
 
+file(GLOB_RECURSE RL
+    RELATIVE ${CMAKE_SOURCE_DIR}
+    /src/*.ragel
+)
+
 RAGEL_TARGET(cli
     lib/cli/src/cli.ragel
     ${CMAKE_SOURCE_DIR}/tmp/cli.ragel.cpp
