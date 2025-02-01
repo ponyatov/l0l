d8b11fec5e492316d29ea70d9f10294d914261db
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 23:19:52 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/clean.cmake
+++ cmake/clean.cmake
@@ -1,12 +1,3 @@
-# add_custom_target(clean_all
-#     COMMAND rm -rf
-#         ${ELF} ${DFU}
-#         ${CMAKE_INSTALL_PREFIX}/${CMAKE_PROJECT_NAME}*
-#         ${CMAKE_BINARY_DIR}
-#         ${TMP}/${CMAKE_PROJECT_NAME}*
-#         ${TMP}/*.ragel.*
-# )
-
 set_property (
     TARGET ${CMAKE_PROJECT_NAME} APPEND
     PROPERTY ADDITIONAL_CLEAN_FILES
