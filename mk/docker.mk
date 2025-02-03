d5f9c2e06b59ed4c662737113cb8aa95961233e5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-03 16:06:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/docker.mk
+++ mk/docker.mk
@@ -2,7 +2,8 @@
 
 .PHONY: docker
 docker: Dockerfile $(BIN)/$(BINFILE)
-	docker build -f $< . -t $(MODULE):$(BRANCH)
+	docker build -f $< .                      \
+          -t $(MODULE):$(BRANCH)
 	docker run   -it                          \
         -e MODULE=$(MODULE) -e APP=$(BINFILE) \
-        --rm $(MODULE):$(BRANCH) $(DOCMD)
+        --rm $(MODULE):$(BRANCH)   $(DOCMD)
