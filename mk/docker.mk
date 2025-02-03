fa651e7accb774770872b2bff850f0f605cc749e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-03 16:04:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/docker.mk
+++ mk/docker.mk
@@ -1,9 +1,8 @@
-DOCMD = ./bin/$(BINFILE) ./lib/$(MODULE).ini
+# DOCMD = ./bin/$(BINFILE) ./lib/$(MODULE).ini
 
 .PHONY: docker
 docker: Dockerfile $(BIN)/$(BINFILE)
 	docker build -f $< . -t $(MODULE):$(BRANCH)
 	docker run   -it                          \
         -e MODULE=$(MODULE) -e APP=$(BINFILE) \
-        --rm  $(MODULE):$(BRANCH)             \
-        $(DOCMD)
+        --rm $(MODULE):$(BRANCH) $(DOCMD)
